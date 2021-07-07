f7254713c587ed075b86854904a16665fa6f03ad
Parth Patel <Parth.patel.r@gmail.com>
2021-07-07 07:55:11 -0400

    table

--- src/components/Equipments/ViewEquipments.vue
+++ src/components/Equipments/ViewEquipments.vue
@@ -1,29 +1,31 @@
 <template>
   <v-row d-flex align-content-space-around class="mx-auto">
-    <v-col v-for="equip in equipments" :key="equip.equipment_id" cols="4">
+    <v-col>
       <v-card>
-        <v-card-title primary-title>
-          {{ equip.info.id }}
+        <v-card-title>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
         </v-card-title>
-        <v-card-subtitle>
-          {{
-            equip.vinData.Make.toUpperCase() +
-              " " +
-              equip.vinData.Model.toUpperCase()
-          }}
-        </v-card-subtitle>
-        <v-card-subtitle>{{ equip["Model Year"] }}</v-card-subtitle>
-        <v-card-actions>
-          <v-btn
-            icon
-            :to="{
-              name: 'Equipment',
-              params: { id: equip.equipment_id, equipment: equip },
-            }"
-            @click="console(equip)"
-            ><v-icon>mdi-pencil</v-icon></v-btn
-          >
-        </v-card-actions>
+        <v-data-table
+          :headers="headers"
+          :items="equipments"
+          :search="search"
+          @click:row="handleClick"
+        >
+          <template v-slot:item.actions="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon small @click="deleteItem(item)">
+              mdi-delete
+            </v-icon>
+          </template></v-data-table
+        >
       </v-card>
     </v-col>
   </v-row>
@@ -36,8 +38,11 @@ export default {
     this.getEquipments();
   },
   methods: {
-    console(x) {
-      console.log(x);
+    handleClick: (equip) => {
+      this.$router.push({
+        name: "Equipment",
+        params: { id: equip.equipment_id, equipment: equip },
+      });
     },
     async getEquipments() {
       this.equipments = [];
@@ -76,6 +81,18 @@ export default {
   data() {
     return {
       equipments: [],
+      search: "",
+      headers: [
+        {
+          text: "Truck ID",
+          align: "start",
+          value: "info.id",
+        },
+        { text: "Make", value: "vinData.Make" },
+        { text: "Year", value: "vinData.Model Year" },
+        { text: "Model", value: "vinData.Model" },
+        { text: "Mileage", value: "info.mileage" },
+      ],
     };
   },
 };
